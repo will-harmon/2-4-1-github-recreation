6b4cd16709b98997543444cd2e27f133f2beb7e8
Will Harmon <willcharmon@gmail.com>
2019-09-23 16:28:07 -0400

    final

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 $(function() {
 
   const BASE_URL = 'https://api.github.com/users/will-harmon';
+  const ORGS_URL = 'https://api.github.com/users/will-harmon/orgs';
+  const REPOS_URL = 'https://api.github.com/users/will-harmon/repos';
   const USERNAME = 'will-harmon';
   const CLIENT_ID = 'a0d1fd0e40189d9437d2';
   const CLIENT_SECRET = 'a0d1fd0e40189d9437d2';
@@ -27,10 +29,65 @@ $(function() {
     let context = res.data;
     let html = template(context);
     // console.log(html);
+    //taking html that handlebars created for me and is putting it .profile-data
     $('.profile-data').html(html);
   }
 
   getProfileData.done(createProfileHTML)
+
+
+
+
+
+
+
+//repositories
+  let getRepoData = $.ajax({
+    method: "GET",
+    url: "https://api.github.com/users/will-harmon/repos",
+    dataType: "jsonp",
+    data: {
+      CLIENT_ID: "a0d1fd0e40189d9437d2",
+      CLIENT_SECRET: "bb8cd30ced3f39c4a5fcf1fbf0e7b6c3ac04eec6"
+    }
+  });
+
+  let createRepoHTML = (res) => {
+    console.log('res', res);
+
+  let source = $('#repositories-template').html();
+  let template = Handlebars.compile(source);
+  let context = res.data;
+  let html = template({repos:context});
+
+  $('.repositories-data').html(html);
+}
+
+  getRepoData.done(createRepoHTML)
+
+  //organizations
+  let getOrgData = $.ajax({
+    method: "GET",
+    url: "https://api.github.com/users/will-harmon/orgs",
+    dataType: "jsonp",
+    data: {
+      CLIENT_ID: "a0d1fd0e40189d9437d2",
+      CLIENT_SECRET: "bb8cd30ced3f39c4a5fcf1fbf0e7b6c3ac04eec6"
+    }
+  });
+
+  let createOrgHTML = (res) => {
+    console.log('res', res);
+
+  let source = $('#organizations-template').html();
+  let template = Handlebars.compile(source);
+  let context = res.data;
+  let html = template({orgs:context});
+
+  $('.organizations-data').html(html);
+}
+
+  getOrgData.done(createOrgHTML)
 
 
 
